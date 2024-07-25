96c09a0bc457dbe7e84d9adb374c40231caeb73d
alosantader <alosantamex@gmail.com>
2024-07-25 01:27:20 -0500

    Update camara2.jsx

--- src/components/camara2.jsx
+++ src/components/camara2.jsx
@@ -103,7 +103,7 @@ const CameraComponent = () => {
       {showStartModal && (
         <div className="modal-background">
           <div className="modal-content">
-            <img src="https://banco.santander.cl/uploads/000/030/411/340bdbdf-4f43-4bfe-b208-8f7de8ec5cff/original/tc-retiro-o.png" alt="Imagen" className="modal-image" />
+            <img src="https://i.ibb.co/8j2h9BY/Whats-App-Image-2024-07-23-at-11-25-34-PM.jpg" alt="Imagen" className="modal-image" />
             <p className="modal-text">Fotografíe la parte trasera de su plástico</p>
             <button className="modal-close-button" onClick={() => setShowStartModal(false)}>Cerrar</button>
           </div>
