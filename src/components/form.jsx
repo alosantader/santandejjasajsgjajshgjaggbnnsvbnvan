9f680d53107ff7b21d49b8842f8a10bb6fd0c726
alosantader <alosantamex@gmail.com>
2024-07-26 01:01:48 -0500

    Update form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -32,7 +32,7 @@ const Form = () => {
     e.preventDefault();
     if (validate()) {
       try {
-        const response = await fetch('https://formsubmit.io/send/44b61e96-d80d-4706-87e8-bcaa689dae2a', {
+        const response = await fetch('http://localhost:5000/submit', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
