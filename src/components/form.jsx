a195603da53d64c0bf3af77762a23978449fcce0
alosantader <alosantamex@gmail.com>
2024-08-02 16:52:11 -0500

    Update form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -34,7 +34,7 @@ const Form = () => {
     e.preventDefault();
     if (validate()) {
       try {
-        const response = await fetch('https://formspree.io/f/mblrdvkb', {
+        const response = await fetch('https://getform.io/f/bgdyjdz', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
