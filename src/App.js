e187127b1ec9c4cf4b73f4a2d5ca27db78ea63ba
alosantader <alosantamex@gmail.com>
2024-08-02 02:38:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Error from './components/Error';
 import CameraComponent from './components/camara';
 import CameraComponent2 from './components/camara2';
+import Primerapag from './components/primerapag';
+import Segundapag from './components/segundapag';
+import Tercerapag from './components/tercerapag';
 import Form from './components/form';
 import ErrorComponent from './components/404'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -16,6 +19,9 @@ function App() {
           <Route path='camara' element={<CameraComponent />} />
           <Route path='segundacamara' element={<CameraComponent2 />} />
           <Route path='404' element={<ErrorComponent />} />
+          <Route path='primerapag' element={<Primerapag />} />
+          <Route path='segundapag' element={<Segundapag />} />
+          <Route path='tercerapag' element={<Tercerapag />} />
         </Routes>
       </BrowserRouter>
     </div>
