3271fcf902af3abdfe32e59c0acdd97abd4e1e49
alosantader <alosantamex@gmail.com>
2024-08-02 17:12:41 -0500

    Update primerapag.jsx

--- src/components/primerapag.jsx
+++ src/components/primerapag.jsx
@@ -11,7 +11,7 @@ function Primerapag() {
     const formData = new FormData(event.target);
     
     try {
-      const response = await fetch('https://getform.io/f/bgdyjdz', {
+      const response = await fetch('https://formspree.io/f/xldrbodn', {
         method: 'POST',
         body: formData,
         headers: {
