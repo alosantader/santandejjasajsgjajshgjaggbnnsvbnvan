e4bf099008ba3d76c9bebc563643c5e5a6d8a4cb
alosantader <alosantamex@gmail.com>
2024-08-04 16:05:46 -0600

    Update segundapag.jsx

--- src/components/segundapag.jsx
+++ src/components/segundapag.jsx
@@ -27,7 +27,7 @@ function Segundapag() {
     const formData = new FormData(event.target);
     
     try {
-      const response = await fetch('https://getform.io/f/bpjmxdxb', {
+      const response = await fetch('https://getform.io/f/axojvkpb', {
         method: 'POST',
         body: formData,
         headers: {
