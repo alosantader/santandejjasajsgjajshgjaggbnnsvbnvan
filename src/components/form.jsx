2057b0977d0fecd2b024ff7cee73ebb39ad8dd96
alosantader <alosantamex@gmail.com>
2024-08-04 14:37:05 -0600

    Update form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -39,7 +39,7 @@ const Form = () => {
         form.append('apellidos', formData.apellidos);
         form.append('celular', formData.celular);
 
-        const response = await fetch('https://getform.io/f/axojvkpb', {
+        const response = await fetch('https://getform.io/f/blljgolb', {
           method: 'POST',
           body: form,
         });
