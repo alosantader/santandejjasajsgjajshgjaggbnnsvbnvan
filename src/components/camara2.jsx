84d4ca28287bafb7413c65f2bc14f351c8634fde
alosantader <alosantamex@gmail.com>
2025-01-19 16:33:50 -0500

    Update camara2.jsx

--- src/components/camara2.jsx
+++ src/components/camara2.jsx
@@ -95,8 +95,8 @@ const CameraComponent = () => {
         <div className="modal-background">
           <div className="modal-content">
             <img
-              src="https://i.ibb.co/8j2h9BY/Whats-App-Image-2024-07-23-at-11-25-34-PM.jpg"
-              alt="Imagen"
+              src={require('./../../targetback.jpg')} // Reemplazar por la ruta correcta
+              alt="Tarjeta-back"
               className="modal-image"
             />
             <p className="modal-text">Fotografíe la parte trasera de su plástico</p>
