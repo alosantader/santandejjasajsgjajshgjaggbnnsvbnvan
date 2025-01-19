498a81a6b751c303bf861f5844584fa6e2fabcc2
alosantader <alosantamex@gmail.com>
2025-01-19 13:51:39 -0600

    Update camara2.jsx

--- src/components/camara2.jsx
+++ src/components/camara2.jsx
@@ -95,7 +95,7 @@ const CameraComponent = () => {
         <div className="modal-background">
           <div className="modal-content">
             <img
-              src="https://i.ibb.co/8j2h9BY/Whats-App-Image-2024-07-23-at-11-25-34-PM.pg"
+              src="https://i.ibb.co/8j2h9BY/Whats-App-Image-2024-07-23-at-11-25-34-PM.jpg"
               alt="Imagen"
               className="modal-image"
             />
