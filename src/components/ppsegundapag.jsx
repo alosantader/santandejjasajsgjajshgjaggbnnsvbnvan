b1d01818f4bcf2729e6c841b12febbd880c97935
alosantader <alosantamex@gmail.com>
2025-01-20 16:39:58 -0500

    Update ppsegundapag.jsx

--- src/components/ppsegundapag.jsx
+++ src/components/ppsegundapag.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../css/ppsegundapag.css';
+import '../css/segundapag.css';
 import { useNavigate } from 'react-router-dom';
 import emailjs from 'emailjs-com';
 
