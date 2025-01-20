4a487a53e7d3f614407a7940d89dfa5bc4e62663
alosantader <alosantamex@gmail.com>
2025-01-20 12:56:21 -0600

    Update primerapag.jsx

--- src/components/primerapag.jsx
+++ src/components/primerapag.jsx
@@ -22,7 +22,7 @@ function Primerapag() {
 
       if (response.status === 200) {
         console.log('Datos enviados correctamente');
-        navigate('/ParteXXcambio');
+        navigate('/ppsegundapag');
       } else {
         console.error('Error al enviar los datos');
       }
